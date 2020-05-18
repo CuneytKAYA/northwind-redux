23059277af2ec90f17ad5f936831ce1605e69568
cuneytokankaya <cuneyt.okan.kaya@hotmail.com>
2020-05-18 12:35:22 +0300

    redux

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -7,6 +7,57 @@ export function getProductsSuccess(categories) {
   };
 }
 
+export function createProductSuccess(product) {
+  //POST
+  return {
+    type: actionTypes.CREATE_PRODUCT_SUCCESS,
+    payload: product,
+  };
+}
+export function updateProductSuccess(product) {
+  //PUT
+  return {
+    type: actionTypes.UPDATE_PRODUCT_SUCCESS,
+    payload: product,
+  };
+}
+
+export function saveProductApi(product) {
+  return fetch("http://localhost:3000/products/" + (product.id || ""), {
+    method: product.id ? "PUT" : "POST",
+    body: JSON.stringify(product),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function saveProduct(product) {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((savedProduct) => {
+        product.id
+          ? dispatch(updateProductSuccess(savedProduct))
+          : dispatch(createProductSuccess(savedProduct));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export async function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+
+  const error = await response.text();
+  throw new Error(error);
+}
+export function handleError(error) {
+  console.error("Bir hata olustu.");
+  throw error;
+}
+
 export function getProducts(categoryId) {
   //API ye baglanacagiz(asenkton operation)
   //dispatch:yakala,ele gecir
